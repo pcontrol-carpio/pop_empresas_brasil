9eef83b94232ae1fab2e5c89c5fa5b1f67251d09
Helder <helderwmarcos@gmail.com>
2022-07-27 16:08:27 -0300

    config, estabelecimento e base

---
+++ empresa.php
@@ -0,0 +1,21 @@
+<?php
+
+require "vendor/autoload.php";
+
+use PopEmpresasBrasil\Empresa;
+use PopEmpresasBrasil\LeitorArquivos;
+
+$leitorArquivos = new LeitorArquivos('empre');
+
+$arquivos = $leitorArquivos->lerArquivos();
+if ($arquivos) {
+    foreach ($arquivos as $arquivo) {
+        $cnae = new Empresa($arquivo);
+        $cnae->popularTabela();
+    }
+}
+
+echo PHP_EOL;
+echo 'Empresa finalizado.';
+
+?>
