c0814eaff5c2e41ee4324a613908d39ec9afca6f
Helder <helderwmarcos@gmail.com>
2022-08-15 15:20:47 -0300

    socios

--- socios.php
+++ socios.php
@@ -5,12 +5,14 @@ require "config.php";
 
 use PopEmpresasBrasil\Socio;
 
-$dir = 'arquivos/K3241.K03200Y9.D20709.SOCIOCSV';
+$dir = '../data/output-extract/';
+//$dir = 'arquivos/K3241.K03200Y9.D20709.SOCIOCSV';
 
 $cnae = new Socio($dir);
 $cnae->popularTabela();
 
 echo PHP_EOL;
 echo 'Sócio finalizado.';
+file_put_contents("{$dir}.txt", "{$dir} - Finalizado em: " . date('d/m/Y H:i:s'), FILE_APPEND);
 
 ?>
--- src/Socio.php
+++ src/Socio.php
@@ -28,17 +28,13 @@ class Socio extends PopEmpresasBrasil
             $bd = new BancoDeDados();
             $bd->conectar();
             foreach ($linhas as $linha) {
-                $empresa = $this->selectEmpresa($bd, $linha[0]);
-                if (!is_null($empresa) && !is_null($empresa)) {
-                    $linha[1] = (int)$linha[1];
-                    $linha[4] = (int)$linha[4];
-                    $linha[5] = $this->formatarData($linha[5]);
-                    $linha[9] = (int)$linha[9];
-                    $linha[10] = (int)$linha[10];
-                    $dados = array_combine($this->colunas, $linha);
-                    $dados['empresa_id'] = $empresa['id'];
-                    $bd->insert($this->tabela, $dados);
-                }
+                $linha[1] = (int)$linha[1];
+                $linha[4] = (int)$linha[4];
+                $linha[5] = $this->formatarData($linha[5]);
+                $linha[9] = (int)$linha[9];
+                $linha[10] = (int)$linha[10];
+                $dados = array_combine($this->colunas, $linha);
+                $bd->insert($this->tabela, $dados);
             }
         }
     }
@@ -46,11 +42,6 @@ class Socio extends PopEmpresasBrasil
     private function formatarData($data)
     {
         return substr($data, 0, 4);
-    }
-
-    private function selectEmpresa(BancoDeDados $bd, string $cnpj_basico)
-    {
-        return $bd->select('empresa', "id, razao_social", "cnpj_basico = '" . $cnpj_basico . "'");
     }
 
 
